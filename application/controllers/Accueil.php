8b31d6534f369d0d8bbc902f327f8e2333a84a58
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-07-04 12:28:29 +0200

    commit midi

--- application/controllers/Accueil.php
+++ application/controllers/Accueil.php
@@ -19,14 +19,14 @@ class Accueil extends CI_Controller {
 	{
 		$this->load->view('common/head');
 		$this->load->view('common/header');
-	    $this->load->view('accueil/home');
+	    $this->load->view('references/references');
 	    $this->load->view('common/footer');
 	}
 	public function contact()
 	{
 		$this->load->view('common/head');
 		$this->load->view('common/header');
-	    $this->load->view('accueil/home');
+	    $this->load->view('contacts/contacts');
 	    $this->load->view('common/footer');
 	}
 }
