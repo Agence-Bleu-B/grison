86f6aa2fba71311fa85868f99138adb68e51f1a7
Corentin Monmasson <corentin.monmasson@gmail.com>
2017-07-04 15:08:24 +0200

    slider

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -18,3 +18,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
   </div>
 </header>
+<!-- SLIDER -->
+<div class="container-fluid">
+  <div id="slider" class="row">
+    <div id="carousel-banner" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <!-- <ol class="carousel-indicators">
+      <li data-target="#carousel-banner" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-banner" data-slide-to="1"></li>
+      <li data-target="#carousel-banner" data-slide-to="2"></li>
+    </ol> -->
+
+    <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img src="<?php echo img_url("slider/slid1.jpg"); ?>" class="">
+          <div class="carousel-caption">
+            <h2 class="text-left">Spécialistes du désenfumage passif</h2>
+            <p class="text-left">
+              De nombreux partenaires nous on fait confiance !
+              <br>
+            </p>
+            <a href="<?php echo site_url("references"); ?>" class="btn btn-primary">Découvrez nos réalisations</a>
+          </div>
+        </div>
+        <div class="item">
+          <img src="<?php echo img_url("slider/slid3.jpg"); ?>" class="">
+          <div class="carousel-caption">
+            <h2 class="text-left">Travail dans de nombreux secteurs d'activités</h2>
+            <p class="text-left lead">Hospitalier, Immobilier, Ephad, Industrie, Lycée...</p>
+          </div>
+        </div>
+        <div class="item">
+          <img src="<?php echo img_url("slider/slid2.jpg"); ?>" class="">
+          <div class="carousel-caption">
+            <h2 class="text-left">Nous intervenons sur toute la france</h2>
+            <a href="<?php echo site_url("contact"); ?>" class="btn btn-primary">Contactez nous</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control hidden-xs hidden-sm" href="#carousel-banner" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control hidden-xs hidden-sm" href="#carousel-banner" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+</div>
--- application/views/references/references.php
+++ application/views/references/references.php
@@ -3,58 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <section id="content" class="container-fluid">
-  <!-- SLIDER -->
-  <div id="slider" class="row">
-    <div id="carousel-banner" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <!-- <ol class="carousel-indicators">
-        <li data-target="#carousel-banner" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-banner" data-slide-to="1"></li>
-        <li data-target="#carousel-banner" data-slide-to="2"></li>
-      </ol> -->
-
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="<?php echo img_url("slider/slid1.jpg"); ?>" class="">
-          <div class="carousel-caption">
-            <h2 class="text-left">Spécialistes du désenfumage passif</h2>
-            <p class="text-left">
-              De nombreux partenaires nous on fait confiance !
-              <br>
-            </p>
-            <a href="<?php echo site_url("accueil/references"); ?>" class="btn btn-primary">Découvrez nos réalisations</a>
-          </div>
-        </div>
-        <div class="item">
-          <img src="<?php echo img_url("slider/slid3.jpg"); ?>" class="">
-          <div class="carousel-caption">
-            <h2 class="text-left">Travail dans de nombreux secteurs d'activités</h2>
-            <p class="text-left lead">Hospitalier, Immobilier, Ephad, Industrie, Lycée...</p>
-          </div>
-        </div>
-        <div class="item">
-          <img src="<?php echo img_url("slider/slid2.jpg"); ?>" class="">
-          <div class="carousel-caption">
-            <h2 class="text-left">Nous intervenons sur toute la france</h2>
-            <a href="<?php echo site_url("accueil/contact"); ?>" class="btn btn-primary">Contactez nous</a>
-          </div>
-        </div>
-      </div>
-
-      <!-- Controls -->
-      <a class="left carousel-control hidden-xs hidden-sm" href="#carousel-banner" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control hidden-xs hidden-sm" href="#carousel-banner" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-    <!-- <img src="https://unsplash.it/2000/500/?random" class="img-responsive"> -->
-    <!-- <img src="img" class="img-responsive"> -->
-  </div>
+  
   <div class="row">
     <div class="col-md-12 col-lg-8 col-lg-offset-2">
       <h2 class="title">Nos Références</h2>
