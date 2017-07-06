f102cad07d8715f3403aaf07da86b86394ea84e0
Corentin Monmasson <corentin.monmasson@gmail.com>
2017-07-06 12:33:09 +0200

    modif title

--- application/views/common/head.php
+++ application/views/common/head.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Désenfumage Grison</title>
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Fontawesome (icons) -->
