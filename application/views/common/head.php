ebe613f58afa1e3c43ff64488a8f79173fb26afb
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-08-17 11:34:27 +0200

    ajout analitycs

--- application/views/common/head.php
+++ application/views/common/head.php
@@ -14,3 +14,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" href="<?php echo css_url("style"); ?>">
   </head>
   <body>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-34413716-34', 'auto');
+    ga('send', 'pageview');
+
+  </script>
